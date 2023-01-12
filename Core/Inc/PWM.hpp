a1eb0272f2f7d8ca1894da4449efcc97b94fb0c8
xboehm <boehmfel@hs-pforzheim.de>
2023-01-12 16:14:18 +0100

    feat: add pwm class
    This adds a rudimental pwm class to control the pwm peripheral.

---
+++ Core/Inc/PWM.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "stm32l4xx_hal.h"
+#include <memory>
+
+class PWM{
+public:
+	PWM(TIM_HandleTypeDef& handle, TIM_TypeDef* timer, uint32_t channel);
+	void start();
+	void stop();
+	void changePulse(unsigned int pulse);
+private:
+	TIM_HandleTypeDef& m_handle;
+	TIM_TypeDef* m_timer;
+	uint32_t m_channel;
+};
---
+++ Core/Src/PWM.cpp
@@ -0,0 +1,22 @@
+#include "PWM.hpp"
+
+PWM::PWM(TIM_HandleTypeDef& handle, TIM_TypeDef* timer, uint32_t channel)
+	: m_handle(handle), m_timer(timer), m_channel(channel)
+{}
+
+void PWM::start(){
+	HAL_TIM_PWM_Start(std::addressof(m_handle), m_channel);
+}
+
+void PWM::stop(){
+	HAL_TIM_PWM_Stop(std::addressof(m_handle), m_channel);
+}
+
+void PWM::changePulse(unsigned int pulse){
+	if(pulse < (m_timer->ARR+1)){
+		m_timer->CCR1 = pulse;
+	}
+	else{
+		//Error handling
+	}
+}
